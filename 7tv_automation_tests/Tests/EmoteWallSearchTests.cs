5d1d0f9f74a996d32a68ce0479cfc2512391d14a
Kaniowski <bartlomiejkaniowski@gmail.com>
2024-04-19 01:59:58 +0200

    Add 3 search filter tests

---
+++ 7tv_automation_tests/Tests/EmoteWallSearchTests.cs
@@ -0,0 +1,58 @@
+using _7tv_automation_tests.Fixtures;
+using _7tv_automation_tests.PageObjects;
+using Xunit.Abstractions;
+
+namespace _7tv_automation_tests.Tests
+{
+    public class EmoteWallSearchTests : IClassFixture<DriverFixture>
+    {
+        private readonly DriverFixture _fixture;
+
+        public static ITestOutputHelper TEST_OUTPUT;//static for simple lazy logging
+        public EmoteWallSearchTests(DriverFixture testFixture, ITestOutputHelper testOutputHelper) 
+        {
+            _fixture = testFixture;
+            TEST_OUTPUT = testOutputHelper;
+        }
+
+
+
+
+        //this test could flake if no emotes with the failing keyword are on the first result page due to a change in ranking
+        [Fact]
+        public void CaseSensitiveFilter_ShouldNotShowCapitalLetters_WhenSearchingLowerCase()
+        {
+            CompareResultsToSearchQuery_WithFilter("omegalul", "OMEGALUL", "Case Sensitive");
+        }
+
+        //this test could flake if no static emotes are on the first result page due to a change in ranking
+        [Fact]
+        public void AnimatedFilter_ShouldNotShowStaticEmote()
+        {
+            CompareResultsToSearchQuery_WithFilter("OMEGALUL", "OMEGALUL", "Animated");
+        }
+
+        //this test could flake if the emote with this tag is not on the first result page due to a change in ranking
+        [Fact]
+        public void IgnoreTagsFilter_ShouldNotShowEmoteWithThisTag()
+        {
+            CompareResultsToSearchQuery_WithFilter("#xdddd", "OMEGALUL", "Ignore Tags");
+
+        }
+
+
+        //should add emoteid string as parameter here, to confirm that you found the right emote
+        private void CompareResultsToSearchQuery_WithFilter(string searchQuery, string badResult, string filter)
+        {
+            EmoteWallPage emoteWallPage = new EmoteWallPage(_fixture.driver);
+
+            emoteWallPage.GoTo();
+            emoteWallPage.FillSearchBar(searchQuery);
+
+            emoteWallPage.ShowSearchFilters();
+            emoteWallPage.ToggleSearchFilter(filter);
+
+            Assert.False(emoteWallPage.CheckIfEmoteIsInSearchResult(badResult));
+        }
+    }
+}
